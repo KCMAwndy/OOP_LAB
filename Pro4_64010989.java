3f2e5e0db2a785698170a5c77533c1215ec08a6d
Attaphon Plianprasert <plianattaphon@gmail.com>
2022-01-10 19:50:49 +0700

    LAB1-4

---
+++ Pro4_64010989.java
@@ -0,0 +1,15 @@
+//Health application
+import java.util.Scanner;
+
+public class Pro4_64010989 {
+    public static void main(String[] args) {
+        System.out.print("Enter weight in pounds : ");
+        Scanner input = new Scanner(System.in);
+        float lb = input.nextFloat();
+        System.out.print("Enter weight in inches : ");
+        float inch = input.nextFloat();
+        input.close();
+        float BMI = (lb*0.45359237F)/(inch*0.0254F*inch*0.0254F);
+        System.out.printf("BMI is %.4f",BMI); 
+    }
+}
